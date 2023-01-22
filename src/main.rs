3222c61d6c43780016ba493fe7498c510d882b00
Mathspy <mathspy257@gmail.com>
2023-01-21 22:48:12 -0500

    Mix game capture with mic and send to blackhole
    SUCCESSFUL FIRST ATTEMPT

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,31 @@
 //! Uses a delay of `LATENCY_MS` milliseconds in case the default input and output streams are not
 //! precisely synchronised.
 
+use std::sync::mpsc::SyncSender;
+
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use ringbuf::HeapRb;
+
+fn create_input_processing_fn(
+    producer: SyncSender<f32>,
+) -> impl Fn(&[f32], &cpal::InputCallbackInfo) {
+    move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        let mut output_fell_behind = false;
+        for &sample in data {
+            if producer.try_send(sample).is_err() {
+                output_fell_behind = true;
+            }
+        }
+        if output_fell_behind {
+            eprintln!("output stream fell behind: try increasing latency");
+        }
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     let host = cpal::default_host();
 
     // Find devices.
-    let input_device = host
+    let microphone = host
         .input_devices()?
         .find(|device| {
             device
@@ -22,6 +39,15 @@ fn main() -> anyhow::Result<()> {
                 .unwrap_or(false)
         })
         .expect("microphone input device exists");
+    let game_capture = host
+        .input_devices()?
+        .find(|device| {
+            device
+                .name()
+                .map(|name| name == "Game Capture HD60 X")
+                .unwrap_or(false)
+        })
+        .expect("game capture input device exists");
     let output_device = host
         .output_devices()?
         .find(|device| {
@@ -32,40 +58,23 @@ fn main() -> anyhow::Result<()> {
         })
         .expect("blackhole output device exists");
 
-    println!("Using input device: \"{}\"", input_device.name()?);
+    println!("Using input device: \"{}\"", microphone.name()?);
+    println!("Using input device: \"{}\"", game_capture.name()?);
     println!("Using output device: \"{}\"", output_device.name()?);
 
     // We'll try and use the same configuration between streams to keep it simple.
     let config: cpal::StreamConfig = microphone.default_input_config()?.into();
 
     // The buffer to share samples
-    let ring = HeapRb::<f32>::new(
-        config
-            .sample_rate
-            .0
-            .try_into()
-            .expect("couldn't convert u32 to usize"),
-    );
-    let (mut producer, mut consumer) = ring.split();
-
-    let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
-        let mut output_fell_behind = false;
-        for &sample in data {
-            if producer.push(sample).is_err() {
-                output_fell_behind = true;
-            }
-        }
-        if output_fell_behind {
-            eprintln!("output stream fell behind: try increasing latency");
-        }
-    };
+    let (producer_mic, consumer_mic) = std::sync::mpsc::sync_channel::<f32>(10_240);
+    let (producer_capture, consumer_capture) = std::sync::mpsc::sync_channel::<f32>(10_240);
 
     let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
         let mut input_fell_behind = false;
         for sample in data {
-            *sample = match consumer.pop() {
-                Some(s) => s,
-                None => {
+            *sample = match (consumer_mic.try_recv(), consumer_capture.try_recv()) {
+                (Ok(s1), Ok(s2)) => s1 + s2,
+                _ => {
                     input_fell_behind = true;
                     0.0
                 }
@@ -81,13 +90,20 @@ fn main() -> anyhow::Result<()> {
         "Attempting to build both streams with f32 samples and `{:?}`.",
         config
     );
-    let input_stream = microphone.build_input_stream(&config, input_data_fn, err_fn)?;
+    let microphone_stream =
+        microphone.build_input_stream(&config, create_input_processing_fn(producer_mic), err_fn)?;
+    let game_capture_stream = game_capture.build_input_stream(
+        &config,
+        create_input_processing_fn(producer_capture),
+        err_fn,
+    )?;
     let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn)?;
     println!("Successfully built streams.");
 
     // Play the streams.
     println!("Starting the input and output streams",);
-    input_stream.play()?;
+    microphone_stream.play()?;
+    game_capture_stream.play()?;
     output_stream.play()?;
 
     loop {
